8adbbf47aef30ac7a540a9fd039ed0196297af1a
Tim Wurmbrand <wurmbrand.tim@gmail.com>
2026-02-28 22:30:16 +0100

    chore: fix route name

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/dashboard');
+Route::redirect('/', '/dashboard')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
