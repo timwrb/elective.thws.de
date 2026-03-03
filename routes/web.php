a0125a83196b6e44d1b5249f9a0cd4094e1e50ee
Daniel Horman <uhorman@gmail.com>
2026-03-03 21:20:48 +0100

    feat: add AWPM Livewire resources, add to layout tests(add): Livewire tests for AWPF resources

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Livewire\Awpf\Index;
+use App\Livewire\Awpf\Select;
+use App\Livewire\Awpf\Show;
 use App\Livewire\Fwpm\FwpmIndex;
 use App\Livewire\Fwpm\FwpmShow;
 use Illuminate\Support\Facades\Route;
@@ -9,6 +12,14 @@ Route::redirect('/', '/dashboard')->name('home');
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
 
-    Route::livewire('fwpm/{semester}', FwpmIndex::class)->name('fwpm.index');
-    Route::livewire('fwpm/{semester}/{fwpm}', FwpmShow::class)->name('fwpm.show');
+    Route::prefix('fwpm')->name('fwpm.')->group(function () {
+        Route::livewire('/{semester}', FwpmIndex::class)->name('fwpm.index');
+        Route::livewire('{semester}/{fwpm}', FwpmShow::class)->name('fwpm.show');
+    });
+
+    Route::prefix('awpf')->name('awpf.')->group(function () {
+        Route::livewire('/', Index::class)->name('index');
+        Route::livewire('/select', Select::class)->name('select');
+        Route::livewire('/{awpf}', Show::class)->name('show');
+    });
 });
